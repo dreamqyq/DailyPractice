d6c088ac25dd39e8f239dfc465ec05c065ed9860
enoch <qyqdream@gmail.com>
2018-07-12 16:07:28 +0800

    004:新增可传入DOM集合

--- js/004.js
+++ js/004.js
@@ -1,38 +1,48 @@
-window.jQuery = function(nodeOrSelector){
+window.jQuery = function(nodeOrSelector) {
 	let nodes = {}
+	// 传入选择器
 	if (typeof nodeOrSelector === 'string') {
 		var temp = document.querySelectorAll(nodeOrSelector)
-		for(let i = 0; i < temp.length; i++){
+		for (let i = 0; i < temp.length; i++) {
 			nodes[i] = temp[i]
-			nodes['length'] = temp.length
 		}
-	}
-	else if(nodeOrSelector instanceof Node){
-		nodes={
-			0:nodeOrSelector,
-			length : 1
+		nodes['length'] = temp.length
+	} 
+	// 传入节点
+	else if (nodeOrSelector instanceof Node) {
+		nodes = {
+			0: nodeOrSelector,
+			length: 1
 		}
+	} 
+	// 传入DOM集合
+	else {
+		for (let i = 0; i < nodeOrSelector.length; i++) {
+			nodes[i] = nodeOrSelector[i]
+		}
+		nodes['length'] = nodeOrSelector.length
 	}
 
-	nodes.addClass = function(classes){
-		for(let key in classes){
-			for(let i = 0;i < nodes.length; i++){
+	nodes.addClass = function(classes) {
+		for (let key in classes) {
+			for (let i = 0; i < nodes.length; i++) {
 				let methodName = classes[key] ? 'add' : 'remove'
 				nodes[i].classList[methodName](key)
 			}
-			
+
 		}
-		
+
 	}
-	nodes.Text = function(text){
+
+	nodes.Text = function(text) {
 		var texts = []
-		if(text === undefined){
-			for(let i = 0;i<nodes.length; i++){
+		if (text === undefined) {
+			for (let i = 0; i < nodes.length; i++) {
 				texts[i] = nodes[i].textContent
 			}
 			return texts
 		}
-		for(let i = 0;i < nodes.length; i++){
+		for (let i = 0; i < nodes.length; i++) {
 			nodes[i].textContent = text;
 		}
 	}
@@ -43,11 +53,15 @@ window.jQuery = function(nodeOrSelector){
 
 window.$ = window.jQuery
 
-
+var boxes = document.getElementsByTagName('div')
 console.log($(box).Text.call($(box)))
 console.log($('div').Text.call($('div')))
 
-btn.onclick = function () {
-	$('div').addClass.call($('div'),{red:true,box:true,green:false})
-	$('div').Text.call($('div'),'hi')
+btn.onclick = function() {
+	$('div').addClass.call($('div'), {
+		red: true,
+		box: true,
+		green: false
+	})
+	$(boxes).Text.call($('div'), 'hi')
 }
