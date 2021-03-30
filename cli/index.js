6b04dc2e5be7df92a0bbb694bcdde58a33497817
EnochQin <qyqdream@gmail.com>
2021-03-30 17:54:07 +0800

    cli finshed: add writeHTMLFile function

--- cli/index.js
+++ cli/index.js
@@ -9,6 +9,7 @@ const getCommandArgv = () => {
     js: args.includes("js"),
     css: args.includes("css"),
     jsx: args.includes("jsx"),
+    html: args.includes("html"),
     name: ""
   };
   const nameIndex = args.indexOf("--name");
@@ -47,29 +48,41 @@ const writeWhiteFile = async (type, nextIndex) => {
   const fileName = `${nextIndex}.${type}`;
   const filePath = path.join(__dirname, `../${pathHash[type]}/${fileName}`);
   await db.write(filePath, "");
-  consoleSuccess(`${fileName} write success!`);
+  consoleSuccess(`${fileName} create success!`);
+};
+
+const writeHTMLFile = async (nextIndex, commandArgs) => {
+  const styleLink = `\n  <link rel="stylesheet" href="../style/${nextIndex}.css">`;
+  const jsScript = `\n  <script src="../js/${nextIndex}.js"></script>`
+  const jsxScript = `\n  <script type="text/babel" src="../js/${nextIndex}.jsx"></script>`
+  const HTMLTemplate = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${commandArgs.name}</title>${commandArgs.css ? styleLink : ""}
+</head>
+<body>${commandArgs.jsx ? jsxScript : ""}${commandArgs.js ? jsScript : ""}
+</body>
+</html>
+  `;
+  const filePath = path.join(__dirname, `../pages/${nextIndex}.html`);
+  await db.write(filePath, HTMLTemplate);
+  consoleSuccess(`${nextIndex}.html create success!`);
 };
 
 const main = async () => {
   // node cli css js html jsx --name "Practice Name 1"
-  /**
-   * 1. get command arguments ✅
-   * 2. get Next index ✅
-   * 2. write README.md ✅
-   * 3. write css ✅
-   * 4. write js ✅
-   * 5. write jsx ✅
-   * 6. write html（title）, import js、css、jsx
-   * 7. write index.html,add new practice file
-   */
   const args = getCommandArgv();
   if (args) {
     const readmeContent = await db.read(path.join(__dirname, "../README.md"));
     const nextIndex = await getNextPracticeIndex(readmeContent);
     await writeInREADME(readmeContent, nextIndex, args.name);
-    args.js && writeWhiteFile("js", nextIndex);
-    args.jsx && writeWhiteFile("jsx", nextIndex);
-    args.css && writeWhiteFile("css", nextIndex);
+    args.js && await writeWhiteFile("js", nextIndex);
+    args.jsx && await writeWhiteFile("jsx", nextIndex);
+    args.css && await writeWhiteFile("css", nextIndex);
+    args.html && (await writeHTMLFile(nextIndex, args));
   }
 };
 
