170888daec1928dd6421f04ef3cb82ae9e654a3d
EnochQin <qyqdream@gmail.com>
2021-04-13 22:02:14 +0800

    022: add deepCopy4: can copy Date, RegExp

--- js/022.js
+++ js/022.js
@@ -65,6 +65,42 @@ const findCache = (caches, source) => {
   }
 };
 
+// 递归克隆（考虑环）,可以复制 Date，RegExp
+const deepCopy4 = source => {
+  const caches = [];
+  const deepCopy4Inner = source => {
+    if (source instanceof Object) {
+      let cacheDist = findCache(caches, source);
+      if (cacheDist) {
+        return cacheDist;
+      } else {
+        let dist;
+        if (source instanceof Array) {
+          dist = new Array();
+        } else if (source instanceof Function) {
+          dist = () => {
+            return source.apply(this, arguments);
+          };
+        } else if (source instanceof RegExp) {
+          dist = new RegExp(source.source, source.flags);
+        } else if (source instanceof Date) {
+          dist = new Date(source);
+        } else {
+          dist = new Object();
+        }
+        caches.push([source, dist]);
+        for (let key in source) {
+          dist[key] = deepCopy4Inner(source[key]);
+        }
+        return dist;
+      }
+    } else {
+      return source;
+    }
+  };
+  return deepCopy4Inner(source);
+};
+
 const main = () => {
   const obj = { a: 1, b: { x: 10 } };
   const fun = (x, y) => x + y;
@@ -82,6 +118,20 @@ const main = () => {
   const result31 = deepCopy3(obj3);
   console.log(result31);
   console.log(result31.self === obj3.self);
+  const obj41 = { c: 10, d: obj };
+  obj41.self = obj41;
+  const result41 = deepCopy4(obj41);
+  console.log('result41', result41);
+  console.log('result41', result41.self === obj41.self);
+  const obj42 = new Date();
+  const result42 = deepCopy4(obj42);
+  console.log('result42', result42 === obj42);
+  console.log('result42', result42.getTime() === obj42.getTime());
+  const obj43 = /hi+/gi;
+  const result43 = deepCopy4(obj43);
+  console.log('result43', result43 === obj43);
+  console.log('result43', result43.source === obj43.source);
+  console.log('result43', result43.flags === obj43.flags);
 };
 
 main();
